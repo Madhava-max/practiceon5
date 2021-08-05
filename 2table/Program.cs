ae4e276b3d4ffbbf756a2641cd200c02e3b24be4
Madhava-max <kothapallimadhavareddy@gmail.com>
2021-08-05 10:09:02 +0530

    2table

---
+++ 2table/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2table
+{
+    class Program
+    {
+        static void Tableof2(int n)
+        {
+            int num = 1;
+            for(int i=1;i<=n;i++)
+            {
+                num *= 2;
+                Console.WriteLine("2 power of"+i+" = "+num);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give a value up to which you need a table of 2");
+            int number = int.Parse(Console.ReadLine());
+            Tableof2(number);        }
+    }
+}
