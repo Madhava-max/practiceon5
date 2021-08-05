4b3867db145f84f3690209782fa77d7013ff13d4
Madhava-max <kothapallimadhavareddy@gmail.com>
2021-08-05 12:03:48 +0530

    VowelAndConsonant

---
+++ consonant/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace consonant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give a character you want to check");
+            char var = Console.ReadKey().KeyChar;
+            switch (var)
+            {
+                case 'a':
+                    Console.WriteLine("\nthe variable given " +var+ " is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("\nthe variable given " + var + " is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("\nthe variable given " + var + " is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("\nthe variable given " + var + " is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("\nthe variable given " + var + " is vowel");
+                    break;
+                default :
+                    Console.WriteLine("\nthe variable given " + var + " is consonant");
+                    break;
+
+
+            }
+        }
+    }
+}
