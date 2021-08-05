7361f4c43ff813258e33c387cc3f485d84de7ecc
Madhava-max <kothapallimadhavareddy@gmail.com>
2021-08-05 12:49:53 +0530

    prime

---
+++ primefactorization/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace primefactorization
+{
+    class Program
+    {
+        static int checkPrime(int num)
+        {
+            for(int j = 2;j*j<=num;j++)
+            {
+                if(num%j==0)
+                {
+                    return 0;
+                }
+            }
+            return 1;
+
+             
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give a number");
+            int number = int.Parse(Console.ReadLine());
+            for(int i=2;i<=number;i++)
+            {
+                if(number%i==0)
+                {
+                    int result = checkPrime(i);
+                    if(result == 1)
+                    {
+                        Console.WriteLine(""+i);
+                    }
+                }
+            }
+        }
+    }
+}
