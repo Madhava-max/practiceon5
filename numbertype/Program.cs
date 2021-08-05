8928f5dfee3bdbbe020f7e3c7f3ed4380c39e7db
Madhava-max <kothapallimadhavareddy@gmail.com>
2021-08-05 11:21:59 +0530

    evenORodd

---
+++ numbertype/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace numbertype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give the number   ");
+            int number = int.Parse(Console.ReadLine());
+            if(number%2==0)
+            {
+                Console.WriteLine("the number is even ");
+            }
+            else
+            {
+                Console.WriteLine("the number is odd ");
+            }
+        }
+    }
+}
+
+
+     
+    
+
