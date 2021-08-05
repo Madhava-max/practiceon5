efac4cd42d2c50bb6f1b2f1a83e496e7758bed0f
Madhava-max <kothapallimadhavareddy@gmail.com>
2021-08-05 10:28:58 +0530

    swapping

---
+++ swapping/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace swapping
+{
+    class Program
+    {
+        static void swap(int a,int b)
+        {
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("the first number is "+a);
+            Console.WriteLine("the first number is " + b);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the first number is ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("the second number is ");
+            int number2 = int.Parse(Console.ReadLine());
+            swap(number1, number2);
+
+        }
+    }
+}
