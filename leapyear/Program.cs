bd61be91410e7fc64e1c5479765dcb8d3d39d7f7
Madhava-max <kothapallimadhavareddy@gmail.com>
2021-08-05 09:58:46 +0530

    leapyear

---
+++ leapyear/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace leapyear
+{
+    class Program
+    {
+        static void FindingWhetherleapYear(int number)
+        {
+            if(number%400==0)
+            {
+                Console.WriteLine("the given is a leapyear");
+            }
+            if(number%4==0)
+            {
+                if(number%100==0)
+                {
+                    Console.WriteLine("the given is not a leapyear");
+                }
+                else
+                    Console.WriteLine("the given is a leapyear");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the year number");
+            int year = int.Parse(Console.ReadLine());
+             FindingWhetherleapYear(year);
+        }
+    }
+}
