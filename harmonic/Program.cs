dbb21e4f2caa764a5614592cab0217b1d97cd106
Madhava-max <kothapallimadhavareddy@gmail.com>
2021-08-05 10:22:10 +0530

    nth-harmonic

---
+++ harmonic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace harmonic
+{
+    class Program
+    {
+        static void Nthharmonicnumber(int n)
+        {
+            int num = 1;
+            Console.WriteLine("the result is "+num+"/"+n);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give a number for nth term of a harmonic number");
+            int number = int.Parse(Console.ReadLine());
+            Nthharmonicnumber(number);
+        }
+    }
+}
